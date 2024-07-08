0015ef2e09920400130e28d9e3c5c5a6ceb3144a
Abhivita <abhiramvita8@gmail.com>
2024-07-08 14:04:50 +0530

    Screenshots Were Added

--- lib/app/custom_files.dart/json.dart
+++
@@ -1,9 +0,0 @@
-// // var totalSale = [
-// //   {"title": "Total", "subtitle": "2250"},
-// //   {"title": "Advance", "subtitle": "1250"},
-// //   {"title": "Balance", "subtitle": "1000"},
-// // ];
-// var salesDetails = [
-//   {"h1": "Name", "name": "Gajula Krishna","h2": "Name", "phone": "9849410172",}
-// ];
-
